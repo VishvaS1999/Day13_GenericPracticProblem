6641b7d2fbe763ef940ddabe517a8b215fc637f2
Vishva <gvishva2601@gmail.com>
2022-11-06 12:16:00 +0530

    Welcome Generic Find Max Value

---
+++ src/com/Generic/Welcome_Generic_Max_Value.java
@@ -0,0 +1,7 @@
+package com.Generic;
+
+public class Welcome_Generic_Max_Value {
+    public static void main(String[] args) {
+        System.out.println("Welcome Generic Find Maximum number Program:");
+    }
+}
